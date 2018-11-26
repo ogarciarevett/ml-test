0f7028b0014d91cf70a4436574335fe1ebbf6079
Omar Garcia <omar.garcia@truenorth.co>
2018-11-26 00:24:04 -0300

    fix RE parser

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,4 +1,4 @@
-import queryString from "query-string";
+import queryString from 'query-string';
 
 export const convertQueryToString = query => {
   const { search } = queryString.parse(query);
@@ -6,18 +6,19 @@ export const convertQueryToString = query => {
 };
 
 export const parseAmount = amount => {
-  const str = amount.toString().split(".");
+  const str = amount.toString().split('.');
   if (str[0].length >= 5) {
-    str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, "$1.");
-  }
-  if (str[1] && str[1].length >= 5) {
-    str[1] = str[1].replace(/(\d{3})/g, "$1 ");
+    str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1.');
+  } else if (str[1] && str[1].length >= 5) {
+    str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+  } else {
+    str[0] = str[0].replace(/(.)(?=(.{3})+$)/g, '$1.');
   }
 
   return str.shift();
 };
 
 export const parseDecimals = decimal => {
-  if (decimal === 0) return "00";
+  if (decimal === 0) return '00';
   return decimal;
 };
